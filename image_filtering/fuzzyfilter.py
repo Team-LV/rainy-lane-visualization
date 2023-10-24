1a2433d644ffa35ca3bbb8852fd2680654d11115
mineunyoung <mey990425@gmail.com>
2023-10-25 04:57:05 +0900

    Fix: fuzzyfilter.py 코드 오류 수정

--- image_filtering/fuzzyfilter.py
+++ image_filtering/fuzzyfilter.py
@@ -191,27 +191,3 @@ def filtersigma(s1,s2):
         elif(s2=='VL'):
             k=m
     return k
-
-    img = np.float64(img) + 1.0
-    img_retinex = singleScaleRetinex(img, variance)
-    for i in range(img_retinex.shape[2]):
-        unique, count = np.unique(np.int32(img_retinex[:, :, i] * 100), return_counts=True)
-        for u, c in zip(unique, count):
-            if u == 0:
-                zero_count = c
-                break
-        low_val = unique[0] / 100.0
-        high_val = unique[-1] / 100.0
-        for u, c in zip(unique, count):
-            if u < 0 and c < zero_count * 0.1:
-                low_val = u / 100.0
-            if u > 0 and c < zero_count * 0.1:
-                high_val = u / 100.0
-                break            
-        img_retinex[:, :, i] = np.maximum(np.minimum(img_retinex[:, :, i], high_val), low_val)
-        
-        img_retinex[:, :, i] = (img_retinex[:, :, i] - np.min(img_retinex[:, :, i])) / \
-                               (np.max(img_retinex[:, :, i]) - np.min(img_retinex[:, :, i])) \
-                               * 255
-    img_retinex = np.uint8(img_retinex)        
-    return img_retinex
